b64c061f0f88ccf258f4f218c411473bbb6d82a0
Carlos Tavares <carlostavares2350@gmail.com>
2023-03-09 17:19:44 -0300

    Adicionado Menu e while para repetição do Menu principal.

--- index.js
+++ index.js
@@ -4,47 +4,58 @@ const leia = require('prompt-sync')();
 
 // npm i prompt-sync :digitar isso no termonal para baixar o pacote necessário para fazer o prompt
 
+do {
+  console.log("\n\n=======================================================================================\n\n   Digite uma opcao: \n\n [1] - Cadastrar Aluno(a) e sua nota \n\n [2] - Consultar uma nota do aluno(a) \n\n [3] - Relatorio de notas dos alunos(as)\n\n [0] - SAIR DO PROGRAMA\n\n=======================================================================================\n\n");
+  var opcao = parseFloat(leia('Opção: '));
 
-console.log("Digite uma opcao: \n [1] - Cadastrar Aluno \n [2] - Consulta aluno ");
-let opcao = leia();
 
-switch(opcao){
+  switch(opcao){
 
- case '1': 
-  console.log("Digite seu nome: ");
-  var nomeAluno = leia();
+  case 1: 
+    console.log("Digite o nome do aluno: ");
+    var nomeAluno = leia();
 
-  console.log("Digite a sua nota: ");
-  var notaAluno = parseFloat(leia());
+    console.log("Digite a sua nota: ");
+    var notaAluno = leia();
 
-  function cadastraAluno(aluno,ponto){
-  notasDeALunos.push({nome:aluno,nota:ponto})
-  }
+    function cadastraAluno(aluno,ponto){
+    notasDeALunos.push({nome:aluno,nota:ponto})
+    }
 
-  cadastraAluno(nomeAluno,notaAluno);
-  console.log("Aluno(a) Cadasatrado com Sucesso !!")
+    cadastraAluno(nomeAluno,notaAluno);
+    console.log("Aluno(a) Cadasatrado com Sucesso !!")
   break;
 
- case '2':
+  case 2:
 
-  function exibeNomeENota(aluno) {
-    for(i = 0; i < notasDeALunos.length; i++){
+    function exibeNomeENota(aluno) {
+      for(i = 0; i < notasDeALunos.length; i++){
 
-      if (notasDeALunos[i].nome == aluno ){
-        const notaDoAluno = notasDeALunos[i].nota;
-        console.log(`O aluno ${aluno} tem a nota ${notaDoAluno}.`);
-      } if(i==notasDeALunos.length) {
-        console.log("Aluno não encontrado!");
+        if (notasDeALunos[i].nome == aluno ){
+          const notaDoAluno = notasDeALunos[i].nota;
+          console.log(`O aluno ${aluno} tem a nota ${notaDoAluno}.`);
+        } if(i==notasDeALunos.length) {
+          console.log("Aluno não encontrado!");
+        }
       }
     }
-  }
-  console.log("Digite o nome do aluno: ");
-  var nomeAluno = leia();
+    console.log("Digite o nome do aluno: ");
+    var nomeAluno = leia();
 
-  exibeNomeENota(nomeAluno);
+    exibeNomeENota(nomeAluno);
+  break;
+
+  case 3: 
+
+    for(i = 0; i < notasDeALunos.length; i++){
+    console.log(`- O aluno(a) ${notasDeALunos[i].nome} tem a nota ${notasDeALunos[i].nota}`)
+      }
+
   break;
 
   default:
-    console.log(`Desculpe, mas não encontramos uma opçãaao correspondente a ${opcao}`);
-
-}
+      console.log(`\n\n=======================================================================================\n\n   Desculpe, mas não encontramos uma opção correspondente ao número ${opcao} digitado.\n\n=======================================================================================\n\n                               Tente novamente !`)
+  
+      break;
+    }
+}while(opcao !== 0);
