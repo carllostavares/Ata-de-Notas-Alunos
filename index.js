85d44bee2173d7f60d460a64c0256353ec6f456b
Carlos Tavares <carlostavares2350@gmail.com>
2023-03-12 19:38:45 -0300

    Solucionando bug da opçao do 2 quando consultado um aluno que nao existe na ata.

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-const notasDeALunos = require('./alunos');
+const ataDeNotasDeALunos = require('./alunos');
 
 const leia = require('prompt-sync')();
 
-// npm i prompt-sync :digitar isso no terminal para baixar o pacote necessário para fazer o prompt
+// npm contador prompt-sync :digitar isso no terminal para baixar o pacote necessário para fazer o prompt
 
 do {
   console.log("\n\n=======================================================================================\n\n   Digite uma opcao: \n\n [1] - Cadastrar Aluno(a) e sua nota \n\n [2] - Consultar uma nota do aluno(a) \n\n [3] - Relatorio de notas dos alunos(as)\n\n [0] - SAIR DO PROGRAMA\n\n=======================================================================================\n\n");
   var opcao = parseFloat(leia('Opção: '));
-
+  var contador = 0;
   switch(opcao){
 
   case 1: 
-    console.log("Digite o nome do aluno: ");
-    var nomeAluno = leia();
+    console.log("\nDigite o nome do aluno(a):\n\n");
+    var nomeAluno = leia('Nome:');
 
-    console.log("Digite a sua nota: ");
-    var notaAluno = leia();
+    console.log("\nDigite a sua nota:\n\n");
+    var notaAluno = leia('Nota:');
 
     function cadastraAluno(aluno,ponto){
-    notasDeALunos.push({nome:aluno,nota:ponto})
+    ataDeNotasDeALunos.push({nome:aluno,nota:ponto})
     }
 
     cadastraAluno(nomeAluno,notaAluno);
-    console.log("Aluno(a) Cadasatrado com Sucesso !!")
+    console.log("\n\nAluno(a) Cadasatrado com Sucesso !!")
   break;
 
   case 2:
 
-  console.log("Digite o nome do aluno:\n");
-  var aluno = leia();
-      for(i = 0; i < notasDeALunos.length; i++){
+  console.log("\nDigite o nome do(a) aluno(a):\n\n");
+  var aluno = leia('Nome:');
 
-        if(notasDeALunos[i].nome == aluno ){
-          const notaDoAluno = notasDeALunos[i].nota;
-          console.log(`O aluno ${aluno} tem a nota ${notaDoAluno}.`);
-          console.log(notasDeALunos.length)
-        } 
-     } if(i-1 == notasDeALunos) {
-      console.log("Nao esta aqui")
+  for(contador = 0; contador < ataDeNotasDeALunos.length; contador++){
+  
+    if(ataDeNotasDeALunos[contador].nome == aluno ){
+     const notaDoAluno = ataDeNotasDeALunos[contador].nota;
+     console.log(`\nAluno(a) ${aluno} tem a nota ${notaDoAluno}.`);
+     contador = ataDeNotasDeALunos.length;
+     } else {
+      if(contador + 1  == ataDeNotasDeALunos.length) {
+        console.log(`\n\nAluno(a) ${aluno} não está cadastrado(a) !`)
+       }
      }
+  }
   break;
 
   case 3: 
 
-    for(i = 0; i < notasDeALunos.length; i++){
-    console.log(`- O aluno(a) ${notasDeALunos[i].nome} tem a nota ${notasDeALunos[i].nota}`)
+    for(contador = 0; contador < ataDeNotasDeALunos.length; contador++){
+    console.log(`\n\n- Aluno(a) ${ataDeNotasDeALunos[contador].nome} tem a nota ${ataDeNotasDeALunos[contador].nota}`)
       }
 
   break;
@@ -54,7 +57,7 @@ do {
   break;
 
   default:
-      console.log(`\n\n=======================================================================================\n\n   Desculpe, mas não encontramos uma opção correspondente ao número ${opcao} digitado.\n\n=======================================================================================\n\n                               Tente novamente !`)
+      console.log(`\n\n=======================================================================================\n\n   Desculpe, mas não encontramos uma opção correspondente ao número ${opcao} digitado.\n\n=======================================================================================\n\n                               Tente novamente !\n ####################################################################################`)
   
       break;
     }
