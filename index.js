6a0c3d49eaefb376cb4d9b94342cf46c17a806ef
Carlos Tavares <carlostavares2350@gmail.com>
2023-03-12 14:33:36 -0300

    Adicionado CASE 0 para opção de sair do programa com formatação de mensagem de despedida.

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const notasDeALunos = require('./alunos');
 
 const leia = require('prompt-sync')();
 
-// npm i prompt-sync :digitar isso no termonal para baixar o pacote necessário para fazer o prompt
+// npm i prompt-sync :digitar isso no terminal para baixar o pacote necessário para fazer o prompt
 
 do {
   console.log("\n\n=======================================================================================\n\n   Digite uma opcao: \n\n [1] - Cadastrar Aluno(a) e sua nota \n\n [2] - Consultar uma nota do aluno(a) \n\n [3] - Relatorio de notas dos alunos(as)\n\n [0] - SAIR DO PROGRAMA\n\n=======================================================================================\n\n");
   var opcao = parseFloat(leia('Opção: '));
-
 
   switch(opcao){
 
@@ -28,21 +27,18 @@ do {
 
   case 2:
 
-    function exibeNomeENota(aluno) {
+  console.log("Digite o nome do aluno:\n");
+  var aluno = leia();
       for(i = 0; i < notasDeALunos.length; i++){
 
-        if (notasDeALunos[i].nome == aluno ){
+        if(notasDeALunos[i].nome == aluno ){
           const notaDoAluno = notasDeALunos[i].nota;
           console.log(`O aluno ${aluno} tem a nota ${notaDoAluno}.`);
-        } if(i==notasDeALunos.length) {
-          console.log("Aluno não encontrado!");
-        }
-      }
-    }
-    console.log("Digite o nome do aluno: ");
-    var nomeAluno = leia();
-
-    exibeNomeENota(nomeAluno);
+          console.log(notasDeALunos.length)
+        } 
+     } if(i-1 == notasDeALunos) {
+      console.log("Nao esta aqui")
+     }
   break;
 
   case 3: 
@@ -51,6 +47,10 @@ do {
     console.log(`- O aluno(a) ${notasDeALunos[i].nome} tem a nota ${notasDeALunos[i].nota}`)
       }
 
+  break;
+
+  case 0:
+      console.log(" >>>>>>>>>>>>>>>>>>>> Programa encerrado ! <<<<<<<<<<<<<<<<<<<<\n >>>>>>>>>>>>>>>>>>>> Obrigado, e até mais !! <<<<<<<<<<<<<<<<<<<<");
   break;
 
   default:
